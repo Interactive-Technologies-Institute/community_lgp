86f1c430d05775ac5d6f03bd8a95bdabbebd332c
Sockitos <jrnogueira@edu.ulisboa.pt>
2024-05-29 16:02:01 +0100

    Improve landing page responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 	$: ({ branding } = data);
 </script>
 
-<div class="container mx-auto mb-20 mt-40 flex flex-col items-center justify-center">
-	<div class="mb-6 h-48 w-48 rounded-full bg-primary"></div>
-	<h1 class="mb-4 text-8xl font-bold">{branding.name}</h1>
-	<p class="text-3xl text-muted-foreground">{branding.slogan}</p>
+<div class="container mx-auto mb-20 mt-20 flex flex-col items-center justify-center md:mt-40">
+	<div class="mb-6 h-32 w-32 rounded-full bg-primary md:h-48 md:w-48"></div>
+	<h1 class="mb-4 text-center text-6xl font-bold md:text-8xl">{branding.name}</h1>
+	<p class="text-center text-xl text-muted-foreground md:text-3xl">{branding.slogan}</p>
 </div>
