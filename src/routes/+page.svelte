9088b447ae896b94ea2dcaa5f11611434eb9a07a
Sockitos <jrnogueira@edu.ulisboa.pt>
2024-05-29 11:31:03 +0100

    Change landing page font sizes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="container mx-auto mb-20 mt-40 flex flex-col items-center justify-center">
-	<div class="h-48 w-48 rounded-full bg-primary"></div>
-	<h1 class="mb-2 text-9xl font-medium">{branding.name}</h1>
+	<div class="mb-6 h-48 w-48 rounded-full bg-primary"></div>
+	<h1 class="mb-4 text-8xl font-bold">{branding.name}</h1>
 	<p class="text-3xl text-muted-foreground">{branding.slogan}</p>
 </div>
