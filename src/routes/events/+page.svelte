007a3a9324afd1743aec8cb7db9b7caf533af46e
Sockitos <jrnogueira@edu.ulisboa.pt>
2024-07-05 15:28:22 +0100

    Improve create buttons

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -11,15 +11,15 @@
 </script>
 
 <PageHeader title="Events" subtitle="Find & share events" />
-<div class="container mx-auto flex flex-row justify-between">
+<div class="container mx-auto flex flex-row justify-between gap-x-2">
 	<div class="flex flex-1 flex-row gap-x-2 sm:gap-x-4 md:flex-auto">
 		<Input placeholder="Search..." class="flex-1 sm:max-w-64"></Input>
 		<TagFilterButton tags={data.tags} />
 		<SortButton />
 	</div>
-	<Button href="/events/create">
-		<PlusCircle class="mr-2 h-4 w-4" />
-		Create an Event
+	<Button href="/events/create" class="w-10 p-0 sm:w-auto sm:px-4 sm:py-2">
+		<PlusCircle class="h-4 w-4 sm:mr-2" />
+		<span class="sr-only sm:not-sr-only">Create Event</span>
 	</Button>
 </div>
 <div class="container mx-auto grid grid-cols-1 gap-6 py-10 lg:grid-cols-2">
--- src/routes/how-to/+page.svelte
+++ src/routes/how-to/+page.svelte
@@ -11,15 +11,15 @@
 </script>
 
 <PageHeader title="How to" subtitle="Learn & share how to solve your problems" />
-<div class="container mx-auto flex flex-row justify-between">
+<div class="container mx-auto flex flex-row justify-between gap-x-2">
 	<div class="flex flex-1 flex-row gap-x-2 sm:gap-x-4 md:flex-auto">
 		<Input placeholder="Search..." class="flex-1 sm:max-w-64"></Input>
 		<TagFilterButton tags={data.tags} />
 		<SortButton />
 	</div>
-	<Button href="/how-to/create" class="fixed bottom-6 right-6 sm:static">
-		<PlusCircle class="mr-2 h-4 w-4" />
-		Create a How-to
+	<Button href="/how-to/create" class="w-10 p-0 sm:w-auto sm:px-4 sm:py-2">
+		<PlusCircle class="h-4 w-4 sm:mr-2" />
+		<span class="sr-only sm:not-sr-only">Create How To</span>
 	</Button>
 </div>
 <div
