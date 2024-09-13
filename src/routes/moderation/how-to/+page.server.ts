2917830083d14fa28767cd588872964f7271e035
João Nogueira <jrnogueira@edu.ulisboa.pt>
2024-09-13 15:10:36 +0100

    Implement query params in howto and events moderation

--- src/routes/moderation/how-to/+page.server.ts
+++ src/routes/moderation/how-to/+page.server.ts
@@ -1,17 +1,28 @@
 import { updateModerationInfoSchema } from '@/schemas/moderation-info.js';
 import type { HowToWithModeration } from '@/types/types';
-import { handleFormAction } from '@/utils';
+import { arrayQueryParam, handleFormAction, stringQueryParam } from '@/utils';
 import { error, fail } from '@sveltejs/kit';
 import { setFlash } from 'sveltekit-flash-message/server';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
 export const load = async (event) => {
+	const search = stringQueryParam().decode(event.url.searchParams.get('s'));
+	const tags = arrayQueryParam().decode(event.url.searchParams.get('tags'));
+
 	async function getHowTos(): Promise<HowToWithModeration[]> {
-		const query = event.locals.supabase
+		let query = event.locals.supabase
 			.from('howtos')
 			.select('*, moderation:latest_howtos_moderation!inner(status, inserted_at, comment)')
 			.order('updated_at', { ascending: false });
+
+		if (search) {
+			query = query.textSearch('fts', search, { config: 'simple', type: 'websearch' });
+		}
+
+		if (tags && tags.length) {
+			query = query.overlaps('tags', tags);
+		}
 
 		const { data: howTos, error: howTosError } = await query;
 
@@ -24,8 +35,33 @@ export const load = async (event) => {
 		return howTos;
 	}
 
+	async function getTags(): Promise<Map<string, number>> {
+		const { data: tags, error: tagsError } = await event.locals.supabase
+			.from('howtos_tags')
+			.select();
+
+		if (tagsError) {
+			const errorMessage = 'Error fetching tags, please try again later.';
+			setFlash({ type: 'error', message: errorMessage }, event.cookies);
+			return error(500, errorMessage);
+		}
+
+		const tagMap = new Map<string, number>();
+		if (tags) {
+			tags.forEach((tag) => {
+				const { count, tag: tagName } = tag;
+				if (count !== null && tagName !== null) {
+					tagMap.set(tagName, count);
+				}
+			});
+		}
+
+		return tagMap;
+	}
+
 	return {
 		howTos: await getHowTos(),
+		tags: await getTags(),
 		updateModerationForm: await superValidate(zod(updateModerationInfoSchema), {
 			id: 'update-moderation',
 		}),
