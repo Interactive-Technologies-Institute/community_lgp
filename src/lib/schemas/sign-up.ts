aca8965cf62e92c73681ea74a525b36005abb1d0
João Nogueira <jrnogueira@edu.ulisboa.pt>
2024-10-02 20:30:33 +0100

    Update sign up schema

--- src/lib/schemas/sign-up.ts
+++ src/lib/schemas/sign-up.ts
@@ -4,7 +4,7 @@ export const signUpSchema = z
 	.object({
 		displayName: z
 			.string()
-			.min(1, { message: 'Display name is required' })
+			.min(5, { message: 'Display name must be at least 5 characters' })
 			.max(25, { message: 'Display name must be less than 25 characters' }),
 		email: z.string().email({ message: 'Please enter a valid email address' }),
 		password: z.string().min(8, { message: 'Password must be at least 8 characters' }),
