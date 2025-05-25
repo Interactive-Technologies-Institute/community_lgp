a7639ae2789949093299f74f64320ff8635d3f50
matildetocha <matilde.tocha@tecnico.ulisboa.pt>
2025-05-25 01:11:13 +0100

    fix: update adapter to match Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { preprocessor as documentPreprocessor } from '@sveltekit-addons/document';
 import { mdsvex } from 'mdsvex';
